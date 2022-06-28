2491a46c95aabe89d2b58855c93a809af77c1436
msuthar09 <maharshi.suthar@righteous.ai>
2022-06-28 09:51:14 -0400

    Implemented the use of CLI to get host and name

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""A setuptools based setup module.
+"""A setuptools based general setup module for PyPI.
 See:
 https://packaging.python.org/guides/distributing-packages-using-setuptools/
 https://github.com/pypa/sampleproject
@@ -6,17 +6,33 @@ https://github.com/pypa/sampleproject
 
 # Always prefer setuptools over distutils
 from importlib.metadata import version
+from unicodedata import name
 from setuptools import setup, find_packages
 import pathlib
 import datetime
 import requests
 from requests.structures import CaseInsensitiveDict
+import sys, getopt
 
 here = pathlib.Path(__file__).parent.resolve()
 
-# Get the version number from the datetime and previous versions
+# Get the host and project name from the command line arguments
+argv = sys.argv[1:]
+try:
+    options, arguments = getopt.getopt(argv, "h:n:", ["host=", "name="])
+except getopt.GetoptError:
+    print(
+        "Invalid usage, please use the syntax: setup.py -h <host> -n <name> or setup.py --host <host> --name <name>"
+    )
+    sys.exit(2)
+for opt, arg in options:
+    if opt in ("-h", "--host"):
+        host = arg
+    elif opt in ("-n", "--name"):
+        project_name = arg
 
-url = "https://test.pypi.org/pypi/calltraces/json"
+# Get the version number from the datetime and previous versions
+url = "https://" + host + "/pypi/" + project_name + "/json"
 
 headers = CaseInsensitiveDict()
 headers["Accept"] = "application/json"
@@ -27,17 +43,26 @@ version_number_prefix = datetime.datetime.now().strftime("%Y.%m.%d")
 version_number_suffix = 0
 
 while response == 200:
-    url_prefix = "https://test.pypi.org/pypi/calltraces/"
-    url_suffix = "/".join([(".".join([version_number_prefix, str(version_number_suffix)])), "json"])
-    response = requests.get(url_prefix + url_suffix, headers=headers).status_code
-    version_number = ".".join([version_number_prefix, str(version_number_suffix)])
+    url = (
+        "https://"
+        + host
+        + "/pypi/"
+        + project_name
+        + "/"
+        + version_number_prefix
+        + "."
+        + str(version_number_suffix)
+        + "/json"
+    )
+    response = requests.get(url, headers=headers).status_code
+    version_number = version_number_prefix + "." + str(version_number_suffix)
     version_number_suffix += 1
 
 # Get the long description from the README file
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="calltraces",
+    name=project_name,
     version=version_number,
     description="A small package for tracing function calls",
     long_description=long_description,
@@ -50,8 +75,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "calltraces"},
-    packages=find_packages(where="calltraces"),
+    package_dir={"": project_name},
+    packages=find_packages(where=project_name),
     python_requires=">=3.9",
     project_urls={
         "Bug Reports": "https://github.com/righteouslabs/calltraces/issues",
