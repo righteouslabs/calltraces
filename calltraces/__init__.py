6f846723ca0578e5857f9feb889c3d2a37deeabc
Mistry, Rohit <rohit_mistry@manulife.com>
2022-07-05 11:51:10 -0400

    Major fixes to build system

--- calltraces/__init__.py
+++ calltraces/__init__.py
@@ -6,10 +6,6 @@ commonTraceSettings = traceSettings()
 
 from .linetrace import traceInfo, traceWarning, traceError
 
-from . import _version
-
-__version__ = _version.get_versions()["version"]
-
 
 def replaceLoggingFunctions():
     logging.info = traceInfo
@@ -21,3 +17,8 @@ def replaceLoggingFunctions():
     logging.root.warning = traceWarning
     logging.root.error = traceError
     logging.root.exception = traceError
+
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
